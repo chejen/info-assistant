b33b2ba56d491d8c384168c50090fd1399d41418
chejen <jkopre.qek@gmail.com>
2024-06-07 23:16:19 +0800

    refactor weather info provider

--- src/weather.js
+++ src/weather.js
@@ -5,12 +5,7 @@ import { sendMail } from '../util/sendMail.js';
 const baseUrl = process.env.INFO_WEATHER_BASE_URL;
 const targets = (process.env.INFO_WEATHER_TARGETS || '').split(',');
 
-const converse = (fahrenheit) => {
-  const f = fahrenheit.split('°')[0];
-  return isNaN(f) ? '' : Math.round((f - 32) * 0.5556) + '°C';
-}
-
-const parse = id => fetch(`${baseUrl}${id}`)
+const getData = id => fetch(`${baseUrl}${id}`)
   .then(response => response.text())
   .then(text => {
     const $ = load(text);
@@ -33,13 +28,18 @@ const parse = id => fetch(`${baseUrl}${id}`)
     return Promise.reject(error);
   });
 
-const fetchInfo = async () => {
-  let info = '';
+const convertFahrenheitToCelsius = (fahrenheit) => {
+  const f = fahrenheit.split('°')[0];
+  return isNaN(f) ? '' : Math.round((f - 32) * 0.5556) + '°C';
+}  
+
+const generateTemplate = async () => {
+  let template = '';
   try {
     for (const target of targets) {
       await setTimeout(1234);
-      const { title, data } = await parse(target);
-      info += `
+      const { title, data } = await getData(target);
+      template += `
         <p>
           <h2>${title}</h2>
           <table style="border-collapse: collapse; border: 1px solid black;">
@@ -47,20 +47,27 @@ const fetchInfo = async () => {
           <tr>
             <td>${el.day}</td>
             <td>${el.desc}</td>
-            <td><span style="color: ${+el.precip.split('%')[0] > 30 ? 'red' : 'black'}">precipitation: ${el.precip}</span></td>
-            <td>${converse(el.lowTemp)} - ${converse(el.highTemp)}</td>
+            <td>
+              <span style="color: ${+el.precip.split('%')[0] > 30 ? 'red' : 'black'}">
+                precipitation: ${el.precip}
+              </span>
+            </td>
+            <td>
+              ${convertFahrenheitToCelsius(el.lowTemp)} -
+              ${convertFahrenheitToCelsius(el.highTemp)}
+            </td>
           </tr>`).join('')}
           </table>
         </p>
       `;
     }
   } catch (e) {
-    console.error(`[${new Date()}][ERR] failed to parse.`);
+    console.error(`[${new Date()}][ERR] failed to generate html template.`);
     console.error(e);
   }
 
-  return info;
+  return template;
 }
 
-fetchInfo().then(html =>
-  sendMail({ subject: 'GitHub Actions - Daily Weather Info.', html }));
+generateTemplate()
+  .then(html => sendMail({ subject: '[GitHub Actions] Daily Weather', html }));
